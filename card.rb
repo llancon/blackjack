aefc8697a6ecf3dcbb67e770ea762f8283e77ef7
jkrouse13 <jon.krouse@gmail.com>
2016-10-17 13:45:59 -0400

    ran beautify on card and deck.  Also changed method for adding cards to match rubocops standards

--- card.rb
+++ card.rb
@@ -1,7 +1,7 @@
 class Card
   attr_accessor :suit, :face, :value
 
-  def initialize(face,suit)
+  def initialize(face, suit)
     self.face = face
     self.suit = suit
     self.value = set_value
@@ -18,19 +18,18 @@ class Card
   def set_value
     if %w(J Q K).include? face
       10
-    elsif face == "A"
+    elsif face == 'A'
       11
     else
       face.to_i
     end
   end
 
-  def +(card)
-    value + card.value
+  def +(other)  # changed argument from card to other to match rubocops preferred styling
+    value + other.value
   end
 
   def to_s
     "a #{face} of #{suit}"
   end
-
 end
